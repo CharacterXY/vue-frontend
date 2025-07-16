c6e32118ba41c230e156be16d7f92458e5e1fca4
CharacterXY <webpsyurl@gmail.com>
2025-07-16 22:13:46 +0200

    inicijalni komit vue s login formom

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+  <LoginPage />
+</template>
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.login-heading {
+  display: flex;
+  padding: 1rem;
+  justify-content: center;
+  color: #afafaf;
+  text-align: center;
+  font-size: 1.4rem;
+
+}
+
+.login-form {
+  width: 30rem;
+  height: 25rem;
+  padding: 1rem;
+  background-color: #333;
+  opacity: 0.8;
+}
+
+.sign-up-now {
+  cursor: pointer;
+  color: #42b883;
+  text-decoration: none;
+}
+
+.not-member {
+  display: flex;
+  color: rgb(197, 197, 196);
+  justify-content: flex-start;
+  font-size: .8rem;
+}
+
+.user-email, .user-password {
+  width: 100%;
+  margin-bottom: 1rem;
+  color: #42b883;
+}
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <v-app>
+      <v-main>
+        <v-container class="main-container fill-height d-flex justify-center align-center" >
+          <v-card class="pa-1 login-form">
+            <v-card-title class="login-heading">Login page</v-card-title>
+            <v-card-text>
+              <v-text-field class="user-email" label="Email" v-model="email" />
+              <v-text-field class="user-password" label="Password" type="password" v-model="password" />
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="info" @click="login">Login</v-btn>
+            </v-card-actions>
+            <v-card-text>
+                <div class="not-member">Not a member?<a class="sign-up-now">Sign up now</a></div>
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
